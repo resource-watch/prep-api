86b1c711f75bd8d64e17f8cee49c8a4a7e7ad611
Clément Prod'homme <clement.prodhomme@vizzuality.com>
2016-06-16 13:06:05 +0200

    Adds the wrapper class, the viewport meta tag and basic media queries variables

--- app/assets/javascripts/components/Footer.js
+++ app/assets/javascripts/components/Footer.js
@@ -1,5 +1,11 @@
 import React from 'react';
 
 export default function () {
-  return <div className="c-footer">Footer!</div>;
+  return (
+    <div className="c-footer">
+      <div className="wrapper">
+        Footer!
+      </div>
+    </div>
+  );
 }
