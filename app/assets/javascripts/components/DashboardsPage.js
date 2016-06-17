3c7339f591724d57204d124c12339a93d16cd680
Clément Prod'homme <clement.prodhomme@vizzuality.com>
2016-06-17 12:38:39 +0200

    Adds the "other links" section on the dashboards page

--- app/assets/javascripts/components/DashboardsPage.js
+++ app/assets/javascripts/components/DashboardsPage.js
@@ -8,6 +8,27 @@ export default function () {
     <div className="l-dashboards">
       Welcome to the dashboards!
 
+      <div className="other-links">
+        <div className="wrapper">
+          <div className="cards-container">
+            <div className="card -map">
+              <Title inverse center>Data on the map</Title>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+              <a href="">
+                <Button color={1}>Explore the map</Button>
+              </a>
+            </div>
+            <div className="card -image">
+              <Title inverse center>Insights</Title>
+              <p>Integer id placerat ligula, eget consequat sapien. Duis nec neque scelerisque</p>
+              <a href="">
+                <Button color={1}>Explore the insights</Button>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+
       <div className="pre-header">
         <div className="wrapper">
           <div className="content">
