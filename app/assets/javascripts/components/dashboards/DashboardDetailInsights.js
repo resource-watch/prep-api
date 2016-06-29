8d59d9fd409aa368bbf37da0a8b5f9b3e693e9ba
Clément Prod'homme <clement.prodhomme@vizzuality.com>
2016-06-29 16:34:17 +0200

    Slightly reduces the code of the DashboardDetailInsightsPage component

--- app/assets/javascripts/components/dashboards/DashboardDetailInsights.js
+++ app/assets/javascripts/components/dashboards/DashboardDetailInsights.js
@@ -3,34 +3,30 @@ import { Link } from 'react-router';
 import Card from '../cards/Card';
 import Title from '../commons/Title';
 
-function DashboardDetailIndicators(props) {
-  let content = [];
-  if (props.data) {
-    props.data.forEach((card, index) => {
-      content.push(
-        <Card borderType={1} key={`card-${index}`}>
-          <Link to={`/insights/${card.slug}`}>
-            <Title type="content">
-              {card.title}
-            </Title>
-          </Link>
-          <p className="content">
-            {card.content}
-          </p>
+function DashboardDetailInsights(props) {
+  const content = props.data.map((card, index) => (
+    <Card borderType={1} key={`card-${index}`}>
+      <Link to={`/insights/${card.slug}`}>
+        <Title type="content">
+          {card.title}
+        </Title>
+      </Link>
+      <p className="content">
+        {card.content}
+      </p>
 
-          {card.partner &&
-            <a target="_blank" href={card.partner.href}>
-              <img
-                src={gon.assets[card.partner.logo]}
-                className="logo"
-                alt={card.partner.name}
-              />
-            </a>
-          }
-        </Card>
-      );
-    });
-  }
+      {card.partner &&
+        <a target="_blank" href={card.partner.href}>
+          <img
+            src={gon.assets[card.partner.logo]}
+            className="logo"
+            alt={card.partner.name}
+          />
+        </a>
+      }
+    </Card>
+  ));
+
   return (
     <div className="cards">
       {content}
@@ -38,11 +34,11 @@ function DashboardDetailIndicators(props) {
   );
 }
 
-DashboardDetailIndicators.propTypes = {
+DashboardDetailInsights.propTypes = {
   /**
    * Define dashboard indicators data
    */
-  data: React.PropTypes.any.isRequired
+  data: React.PropTypes.array.isRequired
 };
 
-export default DashboardDetailIndicators;
+export default DashboardDetailInsights;
