83e97c37a5364de14acaee06212b349aceda9680
Clément Prod'homme <clement.prodhomme@vizzuality.com>
2016-06-29 17:01:46 +0200

    Opens a temporary modal when the user clicks the download button of the related datasets

--- app/assets/javascripts/components/commons/RelatedDatasets.js
+++ app/assets/javascripts/components/commons/RelatedDatasets.js
@@ -1,67 +1,117 @@
+import React from 'react';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
+import Modal from './Modal';
 import Title from './Title';
 
-export default function () {
-  return (
-    <div className="c-related-datasets">
-      <div className="wrapper">
-        <Title borderType={1}>Related datasets</Title>
-        <div className="datasets">
-          <div className="dataset">
-            <div className="title">
-              <a href="#">Intact forest landscapes</a>
+export default class RelatedDatasets extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      modalOpen: false
+    };
+  }
+
+  render() {
+    const modal = (
+      <ReactCSSTransitionGroup
+        transitionName="modal"
+        transitionAppear
+        transitionAppearTimeout={300}
+        transitionEnterTimeout={300}
+        transitionLeaveTimeout={300}
+      >
+        {this.state.modalOpen &&
+          <Modal close={() => this.setState({ modalOpen: false })}>
+            <div className="content">
+              The website is under development. Download details will be added
+              here.
             </div>
-            <div className="links">
-              <a href="#">CSV</a>
-              <a href="#">WRI</a>
+          </Modal>
+        }
+      </ReactCSSTransitionGroup>
+    );
+
+    return (
+      <div className="c-related-datasets">
+        <div className="wrapper">
+          <Title borderType={1}>Related datasets</Title>
+          <div className="datasets">
+            <div className="dataset">
+              <div className="title">
+                Intact forest landscapes
+              </div>
+              <div className="links">
+                <a onClick={() => this.setState({ modalOpen: true })}>CSV</a>
+                <a href="http://www.wri.org" rel="noreferrer" target="_blank">
+                  WRI
+                </a>
+              </div>
+            </div>
+            <div className="dataset">
+              <div className="title">
+                Environmental democracy index
+              </div>
+              <div className="links">
+                <a onClick={() => this.setState({ modalOpen: true })}>CSV</a>
+                <a href="http://www.nasa.gov" rel="noreferrer" target="_blank">
+                  NASA
+                </a>
+              </div>
+            </div>
+            <div className="dataset">
+              <div className="title">
+                Epidemic
+              </div>
+              <div className="links">
+                <a onClick={() => this.setState({ modalOpen: true })}>CSV</a>
+                <a
+                  href="http://openweathermap.org"
+                  rel="noreferrer"
+                  target="_blank"
+                >
+                  Open Weather Map
+                </a>
+              </div>
+            </div>
+            <div className="dataset">
+              <div className="title">
+                Aqueduct: Water stress projections
+              </div>
+              <div className="links">
+                <a onClick={() => this.setState({ modalOpen: true })}>CSV</a>
+                <a href="https://www.usgs.gov" rel="noreferrer" target="_blank">
+                  USGS
+                </a>
+              </div>
+            </div>
+            <div className="dataset">
+              <div className="title">
+                Intact forest landscapes
+              </div>
+              <div className="links">
+                <a onClick={() => this.setState({ modalOpen: true })}>CSV</a>
+                <a href="http://www.wri.org" rel="noreferrer" target="_blank">
+                  WRI
+                </a>
+              </div>
+            </div>
+            <div className="dataset">
+              <div className="title">
+                Environmental democracy index
+              </div>
+              <div className="links">
+                <a onClick={() => this.setState({ modalOpen: true })}>CSV</a>
+                <a href="http://www.iucn.org" rel="noreferrer" target="_blank">
+                  IUCN
+                </a>
+              </div>
             </div>
           </div>
-          <div className="dataset">
-            <div className="title">
-              <a href="#">Environmental democracy index</a>
-            </div>
-            <div className="links">
-              <a href="#">CSV</a>
-              <a href="#">NASA</a>
-            </div>
-          </div>
-          <div className="dataset">
-            <div className="title">
-              <a href="#">Epidemic</a>
-            </div>
-            <div className="links">
-              <a href="#">CSV</a>
-              <a href="#">Open Weather Map</a>
-            </div>
-          </div>
-          <div className="dataset">
-            <div className="title">
-              <a href="#">Aqueduct: Water stress projections</a>
-            </div>
-            <div className="links">
-              <a href="#">CSV</a>
-              <a href="#">USGS</a>
-            </div>
-          </div>
-          <div className="dataset">
-            <div className="title">
-              <a href="#">Intact forest landscapes</a>
-            </div>
-            <div className="links">
-              <a href="#">CSV</a>
-              <a href="#">WRI</a>
-            </div>
-          </div>
-          <div className="dataset">
-            <div className="title">
-              <a href="#">Environmental democracy index</a>
-            </div>
-            <div className="links">
-              <a href="#">CSV</a>
-              <a href="#">IUCN</a>
-            </div>
-          </div>
+          {modal}
         </div>
       </div>
-    </div>
-  );
+    );
+  }
+
 }
