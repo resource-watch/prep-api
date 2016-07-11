e1c008f60253595bae84482ad9b078b6d014752f
j8seangel <jseangel@gmail.com>
2016-07-11 16:27:49 +0200

    Handle proxy errors

--- app/controllers/proxy_controller.rb
+++ app/controllers/proxy_controller.rb
@@ -1,7 +1,15 @@
 class ProxyController < ApplicationController
   def index
-    url = URI.parse(params["url"])
-    page = Net::HTTP.get_response(url)
-      render :text => page.body
+    if params["url"]
+      url = URI.parse(params["url"])
+      if url.kind_of?(URI::HTTP)
+        page = Net::HTTP.get_response(url)
+          render :text => page.body
+      else
+        render :text => "Invalid url"
+      end
+    else
+      render :text => "Url param is neccesary"
+    end
   end
 end
