96402464f0b9c67d53df505440c20d0a195dae65
j8seangel <jseangel@gmail.com>
2016-07-22 13:49:36 +0200

    Split logo size in width and height obj

--- app/serializers/api/partner_serializer.rb
+++ app/serializers/api/partner_serializer.rb
@@ -1,5 +1,10 @@
 class Api::PartnerSerializer < ActiveModel::Serializer
 
-  attributes :id, :name, :url, :logo, :logo_dimensions, :contact_name, :contact_email
+  attributes :id, :name, :url, :logo, :logo_size, :contact_name, :contact_email
+
+  def logo_size
+    dimensions = object.logo_dimensions.split('x')
+    { width: dimensions[0].to_i, height: dimensions[1].to_i}
+  end
 
 end
