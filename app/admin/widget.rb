ce9b279f5b000e7e4610083fe3157f789df8deaf
j8seangel <jseangel@gmail.com>
2016-07-27 12:29:54 +0200

    Get widget list and datasets on change

--- app/admin/widget.rb
+++ app/admin/widget.rb
@@ -1,40 +1,35 @@
 ActiveAdmin.register Widget do
 
-  controller do
-    def edit
-      @widget = Widget.find(params[:id])
-
-      conn = Faraday.new(:url => ENV['RW_API_URL']) do |faraday|
-        faraday.request  :url_encoded
-        # faraday.response :logger
-        faraday.adapter  Faraday.default_adapter
-      end
-
-      datasetRequest = conn.get '/datasets', { :app => 'PREP' }
-      @datasets = JSON.parse datasetRequest.body
-      @datasets = @datasets.map{|dc|[dc['name'],dc['id'],{ :selected => dc['id']===@widget.dataset }]}
-
-      if (@widget.dataset)
-        visualizationRequest = conn.get '/datasets' + @widget.dataset
-        @visualization = JSON.parse visualizationRequest.body
-      else
-        @visualization = []
-      end
-    end
-  end
-
   permit_params :title, :slug, :summary, :content, :dataset, :visualization, :data_url, :json_spec, :partner_id, :published
 
-  form do |f|
+  conn = Faraday.new(:url => ENV['RW_API_URL']) do |faraday|
+    faraday.request  :url_encoded
+    # faraday.response :logger
+    faraday.adapter  Faraday.default_adapter
+  end
+
+  datasetRequest = conn.get '/datasets', { :app => 'PREP' }
+  datasets = JSON.parse datasetRequest.body
+
+  form :html => {:id => 'widget_form'} do |f|
+
+    if (f.object.dataset)
+      params = { :app => 'PREP', :datasetId => f.object.dataset, :template => true}
+      visualizationRequest = conn.get '/widgets/'
+      visualization = JSON.parse visualizationRequest.body
+    else
+      visualization = []
+    end
+
     f.semantic_errors
     f.inputs 'Widget Detail' do
+      f.input :dataset, as: :select, collection: datasets.map{|dc|[dc['name'],dc['id'],{ :selected => dc['id']===f.object.dataset }]}
+      f.input :visualization, as: :select, collection: visualization.map{|dc|[dc['name'],dc['id'],{ :selected => dc['id']===f.object.visualization }]}
       f.input :title, required: true
       f.input :slug, required: true
       f.input :summary
       f.input :content
       f.input :data_url, required: true
-      f.input :dataset, as: :select, collection: @datasets
-      f.input :visualization, as: :select, collection: @visualization
       f.input :json_spec, as: :hidden, required: true
       f.input :partner, required: true
       f.input :published, as: :boolean
