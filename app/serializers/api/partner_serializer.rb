44cf06e832ef15443d9f285f040168cbfdadb1b0
David Inga <davidsingal@icloud.com>
2016-08-04 14:50:51 +0200

    added new sizes for partners

--- app/serializers/api/partner_serializer.rb
+++ app/serializers/api/partner_serializer.rb
@@ -1,10 +1,19 @@
 class Api::PartnerSerializer < ActiveModel::Serializer
 
-  attributes :id, :name, :url, :logo, :white_logo, :logo_size, :contact_name, :contact_email
+  attributes :id, :name, :url, :logo, :white_logo, :logo_medium,
+    :white_logo_medium, :logo_size, :contact_name, :contact_email
 
   def logo_size
     dimensions = (object.logo_dimensions || '0x0').split('x')
     { width: dimensions[0].to_i, height: dimensions[1].to_i}
+  end
+
+  def logo_medium
+    object.logo.url(:medium)
+  end
+
+  def white_logo_medium
+    object.white_logo.url(:medium)
   end
 
 end
