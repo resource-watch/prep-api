d167dfa576bcd6b4bfffce5fd9258084eb667bd9
David Inga <davidsingal@icloud.com>
2016-08-24 12:16:42 +0200

    fixing visualisation selection

--- app/admin/widget.rb
+++ app/admin/widget.rb
@@ -35,8 +35,8 @@ ActiveAdmin.register Widget do
 
     f.semantic_errors
     f.inputs 'Widget Detail' do
-      f.input :dataset, as: :select, collection: datasets.map{|dc|[dc['name'],dc['id'],{ :selected => dc['id']===f.object.dataset }]}
-      f.input :visualization, as: :select, collection: visualization.map{|dc|[dc['name'],dc['id'],{ :selected => dc['id']===f.object.visualization }]}
+      f.input :dataset, as: :select, collection: datasets.map{|dc| [dc['name'],dc['id']]}
+      f.input :visualization, as: :select, collection: visualization['data'].map{|vis| [vis['attributes']['name'], vis['id']]}
       div id: "widget-preview"
       f.input :title, required: true
       f.input :slug, required: true
