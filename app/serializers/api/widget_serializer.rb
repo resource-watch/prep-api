887db68f6b225afcb26f2981bfb1789df5cc371c
j8seangel <jseangel@gmail.com>
2016-08-25 12:52:35 +0200

    Add widgets attribution

--- app/serializers/api/widget_serializer.rb
+++ app/serializers/api/widget_serializer.rb
@@ -1,5 +1,5 @@
 class Api::WidgetSerializer < ActiveModel::Serializer
-  attributes :id, :title, :summary, :slug, :content, :data_url, :widget_config
+  attributes :id, :title, :summary, :slug, :content, :attribution, :data_url, :widget_config
 
   def widget_config
      JSON.parse (object.widget_config)
