f7bb3139503f65409ff5ea4777baba8740634fbf
David Inga <davidsingal@icloud.com>
2016-09-22 11:55:01 +0200

    added tool serializer

--- app/serializers/api/tool_serializer.rb
+++ app/serializers/api/tool_serializer.rb
@@ -1,7 +1,15 @@
 class Api::ToolSerializer < ActiveModel::Serializer
 
-  attributes :id, :title, :summary, :url, :attribution
+  attributes :id, :title, :summary, :url, :attribution, :images
 
   belongs_to :partner, serializer: Api::PartnerSerializer
+
+  def images
+    {
+      thumbnail: object.thumbnail.url(:large),
+      logo: object.logo.url(:medium),
+      white_logo: object.white_logo.url(:medium)
+    }
+  end
 
 end
