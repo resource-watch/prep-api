4048e111a20dba27b3495bff8b03527145eedf37
Tiago Santos <santos.tiago@gmail.com>
2017-07-12 17:40:19 +0200

    Changed the cors to allow all operations

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -79,12 +79,4 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-
-  # CORS
-  config.middleware.insert_before 0, Rack::Cors do
-     allow do
-       origins '*'
-       resource '*', :headers => :any, :methods => [:get, :post, :options]
-     end
-   end
 end
