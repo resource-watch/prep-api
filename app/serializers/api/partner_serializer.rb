4db725405157a1625f206bd4d8660c4012614dc2
David Inga <david.inga@vizzuality.com>
2017-07-13 12:23:44 +0200

    support for old site (partners)

--- app/serializers/api/partner_serializer.rb
+++ app/serializers/api/partner_serializer.rb
@@ -3,4 +3,13 @@ class Api::PartnerSerializer < ActiveModel::Serializer
   attributes :id, :name, :partner_type, :description, :content,
              :url, :contact_name, :contact_email, :featured, :published, :thumbnail, :logo, :white_logo
 
+  # Support for old site
+  def images
+    {
+      thumbnail: object.thumbnail.url(:large),
+      logo: object.logo.url(:medium),
+      white_logo: object.white_logo.url(:medium)
+    }
+  end
+
 end
