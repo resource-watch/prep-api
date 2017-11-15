4f8419ac6e8ac565c37046f8829767e7c5af60b9
Ivan Birkman <ivan.yk14@gmail.com>
2017-11-15 10:37:43 +0100

    Fix filter by param all

--- app/models/concerns/filterable.rb
+++ app/models/concerns/filterable.rb
@@ -5,7 +5,7 @@ module Filterable
     def filter(filtering_params)
       results = self.where(nil)
       filtering_params.each do |key, value|
-        results = results.public_send(key, value) if value.present?
+        results = results.public_send(key, value) if value.present? && value != 'all'
       end
       results
     end
