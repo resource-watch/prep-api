2609a6ae1bf53c9109d81906857cf6104cc5d2a1
Ivan Birkman <ivan.yk14@gmail.com>
2017-12-19 17:21:49 +0100

    Add content image

--- app/controllers/api/dashboards_controller.rb
+++ app/controllers/api/dashboards_controller.rb
@@ -1,3 +1,23 @@
 class Api::DashboardsController < ApiController
+  include JSONAPI::Utils
 
+  def create
+    dashboard = Dashboard.new(resource_params)
+    if dashboard.save
+      dashboard.manage_content(request.base_url)
+      jsonapi_render json: dashboard, status: :created
+    else
+      jsonapi_render_errors json: dashboard, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    dashboard = Dashboard.find(params[:id])
+    if dashboard.update(resource_params)
+      dashboard.manage_content(request.base_url)
+      jsonapi_render json: dashboard
+    else
+      jsonapi_render_errors json: dashboard, status: :unprocessable_entity
+    end
+  end
 end
