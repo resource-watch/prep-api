341c41c24ce330928d635221136398c6ab2809d0
Tiago Santos <santos.tiago@gmail.com>
2017-12-21 10:31:36 +0000

    Made the relationship between dashboard and indicators optional

--- app/models/dashboard.rb
+++ app/models/dashboard.rb
@@ -30,7 +30,7 @@ class Dashboard < ApplicationRecord
   validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
 
   belongs_to :partner, optional: :true
-  belongs_to :indicator
+  belongs_to :indicator, optional: :true
 
   accepts_nested_attributes_for :indicator
 
