8bd494bf906036c0df5c2f985e84e76479344a6b
Ivan Birkman <ivan.yk14@gmail.com>
2018-08-16 12:30:02 +0200

    Add proper serializer for dashboard author

--- app/serializers/api/dashboard_serializer.rb
+++ app/serializers/api/dashboard_serializer.rb
@@ -1,6 +1,7 @@
 class Api::DashboardSerializer < ActiveModel::Serializer
   attributes :id, :title, :slug, :summary, :content, :attribution, :published,
-             :user_id, :production, :preproduction, :staging, :tags, :locations, :author
+             :user_id, :production, :preproduction, :staging, :tags, :locations
 
   belongs_to :partner, serializer: Api::PartnerSerializer
+  has_one :author, serializer: Api::AuthorSerializer
 end
